524ea50646350453c34689e3beabc5d9c5616e9f
pKononenko <44846303+pKononenko@users.noreply.github.com>
2020-05-22 19:24:29 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,7 +1,7 @@
 -- Якщо тип не заміняється
-/*DROP VIEW GAMEDATA;
-DROP TYPE publisher_platform_row_table;
-DROP TYPE publisher_platform_row;*/
+-- DROP VIEW GAMEDATA;
+-- DROP TYPE publisher_platform_row_table;
+-- DROP TYPE publisher_platform_row;
 
 
 -- Трохи перероблене VIEW з 3ЛР
